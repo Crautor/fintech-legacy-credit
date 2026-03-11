59ec940e3b88600eefc461c73f33f0c40660170b
João Vitor Dysarz <dysarzjoaovitor@gmail.com>
2026-03-11 20:22:30 -0300

    implementa AnaliseCreditoFactory com Switch Expressions do Java 21

---
+++ src/main/java/br/com/nogueiranogueira/aularefatoracao/service/factory/AnaliseCreditoFactory.java
@@ -0,0 +1,17 @@
+package br.com.nogueiranogueira.aularefatoracao.service.factory;
+
+import br.com.nogueiranogueira.aularefatoracao.dto.TipoConta;
+import br.com.nogueiranogueira.aularefatoracao.strategy.AnaliseStrategy;
+import br.com.nogueiranogueira.aularefatoracao.strategy.AnaliseStrategyPF;
+import br.com.nogueiranogueira.aularefatoracao.strategy.AnaliseStrategyPJ;
+
+public class AnaliseCreditoFactory {
+    // Java 21: Switch Expression com "Arrow syntax" (->)
+    public static AnaliseStrategy obterEstrategia(TipoConta tipo) {
+        return switch (tipo) {
+            case PF -> new AnaliseStrategyPF();
+            case PJ -> new AnaliseStrategyPJ();
+            default -> throw new IllegalArgumentException("Tipo de conta não suportado: " + tipo);
+        };
+    }
+}
