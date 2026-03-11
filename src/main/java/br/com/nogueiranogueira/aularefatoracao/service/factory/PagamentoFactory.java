dd9ac8c9f4d18798eac406a4d8ef07573a3c4722
JoaoVGrande <joaovitorcand@outlook.com>
2026-03-11 20:55:43 -0300

    pagamento facotry
    feito missao do slide do professor

---
+++ src/main/java/br/com/nogueiranogueira/aularefatoracao/service/factory/PagamentoFactory.java
@@ -0,0 +1,18 @@
+package br.com.nogueiranogueira.aularefatoracao.service.factory;
+
+public class PagamentoFactory {
+
+    public static PagamentoStrategy obterEstrategia(String metodo) {
+        if (metodo == null || metodo.trim().isEmpty()) {
+            throw new IllegalArgumentException("Método de pagamento não informado!");
+        }
+
+        return switch (metodo.toUpperCase()) {
+            case "PIX" -> new PixStrategy();
+            case "CARTAO_CREDITO" -> new CartaoCreditoStrategy();
+            case "PAYPAL" -> new PaypalStrategy();
+            case "BOLETO" -> new BoletoStrategy();
+            default -> throw new IllegalArgumentException("Método de pagamento não suportado: " + metodo);
+        };
+    }
+}
---
+++ src/main/java/br/com/nogueiranogueira/aularefatoracao/service/factory/PixStrategy.java
@@ -0,0 +1,11 @@
+package br.com.nogueiranogueira.aularefatoracao.service.factory;
+
+public class PixStrategy implements PagamentoStrategy {
+    @Override
+    public void pagar(double valor) {
+        double valorComDesconto = valor * 0.95;
+        System.out.println("Calculando desconto do PIX...");
+        System.out.println("Gerando chave Copia e Cola.");
+        System.out.println("Pagamento via PIX processado. Total cobrado: R$ " + valorComDesconto);
+    }
+}
