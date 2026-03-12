33ff5f95b4f3dc487866fb4e387b5388ffd81c9c
João Vitor Dysarz <dysarzjoaovitor@gmail.com>
2026-03-11 21:20:58 -0300

    Implementando PagamentoFactory

---
+++ src/main/java/br/com/nogueiranogueira/aularefatoracao/service/factory/PagamentoFactory.java
@@ -0,0 +1,19 @@
+package br.com.nogueiranogueira.aularefatoracao.service.factory;
+
+import br.com.nogueiranogueira.aularefatoracao.strategy.*;
+
+public class PagamentoFactory {
+    public static PagamentoStrategy obterEstrategia(String metodo) {
+        if (metodo == null || metodo.trim().isEmpty()) {
+            throw new IllegalArgumentException("Método de pagamento não informado!");
+        }
+
+        return switch (metodo.toUpperCase()) {
+            case "PIX" -> new PixStrategy();
+            case "CARTAO_CREDITO" -> new CartaoCreditoStrategy();
+            case "PAYPAL" -> new PaypalStrategy();
+            case "BOLETO" -> new BoletoStrategy();
+            default -> throw new IllegalArgumentException("Método de pagamento não suportado: " + metodo);
+        };
+    }
+}
